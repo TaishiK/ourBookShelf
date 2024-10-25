7e56ef97f0ae879b3b777bbe8fb7d18a5a8465be
TaishiK <taishi.kunori@outlook.com>
2024-10-25 15:11:42 +0900

    1st commit from Wsl2Ubuntu.

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("Hello, Rust World!");
-    println!("global: {}", std::env::var("GLOBAL").unwrap());
-    println!("local: {}", std::env::var("LOCAL").unwrap());
+//    println!("global: {}", std::env::var("GLOBAL").unwrap());
+//    println!("local: {}", std::env::var("LOCAL").unwrap());
 }
